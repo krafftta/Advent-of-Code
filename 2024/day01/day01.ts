70f59de0014264f925f75be61e96ca6fd9b410f9
Tamara Krafft <tamara.krafft@uni-a.de>
2024-12-01 14:12:04 +0100

    similarity score for star number two, day01

--- 2024/day01/day01.ts
+++ 2024/day01/day01.ts
@@ -13,7 +13,7 @@ function parse(data: string): [Array<number>, Array<number>] {
     return [listOne, listTwo]
 }
 
-function solve(listOne:Array<number>, listTwo: Array<number>): number {
+function solveStarOne(listOne:Array<number>, listTwo: Array<number>): number {
     let result = 0;
     listOne.sort((a, b) => a - b);
     listTwo.sort((a, b) => a - b);
@@ -25,12 +25,39 @@ function solve(listOne:Array<number>, listTwo: Array<number>): number {
     return result;
 }
 
+function solveStarTwo(listOne:Array<number>, listTwo: Array<number>): number {
+    let result = 0;
+    let counts = {};
+
+    // Iterate over all elements of the left list
+    for (let ele of listOne) {
+        for (let comp of listTwo) {
+            // Count appearances in right list
+            if (ele === comp) {
+                if (counts[ele] === undefined) {
+                    counts[ele] = 1; // Initialize if not already in counts
+                } else {
+                    counts[ele] += 1; // Increment if already exists
+                }
+            }
+        }
+    }
+
+    for (let m in counts) {
+        // Mulitply left element with appearance in right list
+        result += counts[m]* parseInt(m);
+    }
+
+    return result
+}
+
 
 try {
     const data:string = fs.readFileSync('2024/day01/input.txt','utf8');
     let [listOne, listTwo] = parse(data);
-    let result = solve(listOne, listTwo);
-    console.log(result);
+    let resultOne = solveStarOne(listOne, listTwo);
+    let resultTwo = solveStarTwo(listOne, listTwo);
+    console.log(resultOne, resultTwo);
   } catch (err) {
     console.error(err);
   }
