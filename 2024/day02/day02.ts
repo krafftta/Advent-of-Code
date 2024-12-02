9e569c18126b5f4fe60536ad0e6a90f30caa3b90
Tamara Krafft <tamara.krafft@uni-a.de>
2024-12-02 17:57:22 +0100

    day02 star two

--- 2024/day02/day02.ts
+++ 2024/day02/day02.ts
@@ -8,13 +8,23 @@ function parse(data: string): number[][] {
 }
 
 function diff(arr: number[]): number[] {
+    // Get distances between neighbours, inspired by Python's np.diff
     if (arr.length < 2) {
-        return []; // Return an empty array if there are less than 2 elements
+        return [];
+    } else {
+        return arr.slice(1).map((value, index) => value - arr[index]); // or: Array.from({ length: arr.length - 1 }, (_, i) => arr[i + 1] - arr[i]);
     }
-    return arr.slice(1).map((value, index) => value - arr[index]);
+}
+
+function tryRemovingEitherSide(arr: number[], diffArr: number[], removeIndex: number): number {
+    // Tries removing either the current or the next element to see if the preexisting issue can be solved
+    let removeLeft = updatedCheckSortedAndDiff([...arr.slice(0, removeIndex), ...arr.slice(removeIndex + 1)], 1);
+    let removeRight = updatedCheckSortedAndDiff([...arr.slice(0, removeIndex + 1), ...arr.slice(removeIndex + 2)], 1);
+    return Math.max(removeRight, removeLeft);
 }
 
 function checkSortedAndDiff(arr: number[]): number {
+    // Solution for Star One
     let diffArr = diff(arr);
     if (diffArr.some((a) => Math.abs(a) > 3 || a === 0)) {
         return 0;
@@ -24,18 +34,49 @@ function checkSortedAndDiff(arr: number[]): number {
     if (hasBothDirections) {
         return 0;
     }
-
     return 1;
 }
 
-function updatedCheckSortedAndDiff(arr: number[]): number {
-    return 0;
+function updatedCheckSortedAndDiff(arr: number[], tolerate: number = 0): number {
+    // Updated Function for Star Two
+    // Get distances between neighbouring elements
+    let diffArr = diff(arr);
+    // console.log(arr, diffArr)
+
+    // Check if there is an issue with the sorting
+    const hasBothDirections = diffArr.some((a) => a > 0) && diffArr.some((a) => a < 0);
+    if (hasBothDirections) {
+        // The Problem Dampener allows one retry
+        if (tolerate==0) {
+            let removeIndex = 0;
+            if (diffArr.filter((a) => a > 0).length > diffArr.filter((a) => a < 0).length) {
+                removeIndex = diffArr.findIndex((a) =>  a < 0);
+            } else {
+                removeIndex = diffArr.findIndex((a) =>  a > 0);
+            }
+            return tryRemovingEitherSide(arr, diffArr, removeIndex);
+        } else {
+            return 0;
+        }
+    }
+
+    // Check if a diff between neigbours is too large or zero
+    if (diffArr.some((a) => Math.abs(a) > 3 || a === 0)) {
+        if (tolerate==0) {
+            let removeIndex = diffArr.findIndex((a) => Math.abs(a) > 3 || a === 0);
+            return tryRemovingEitherSide(arr, diffArr, removeIndex);
+        } else {
+            return 0;
+        }
+    }
+
+    return 1;
 }
 
 function solveStarOne(levels: number[][]): number {
     let countSafeLevels = 0;
     for (let level of levels) {
-        countSafeLevels += checkSortedAndDiff(level)
+        countSafeLevels += checkSortedAndDiff(level);
     }
     return countSafeLevels;
 }
@@ -43,7 +84,7 @@ function solveStarOne(levels: number[][]): number {
 function solveStarTwo(levels: number[][]): number {
     let countSafeLevels = 0;
     for (let level of levels) {
-        countSafeLevels += updatedCheckSortedAndDiff(level)
+        countSafeLevels += updatedCheckSortedAndDiff(level);
     }
     return countSafeLevels;
 }
@@ -58,10 +99,12 @@ try {
     console.error(err);
 }
 
-console.log(updatedCheckSortedAndDiff([1,2,4,6,7])) // 1
-console.log(updatedCheckSortedAndDiff([1,2,6,7,8])) // 0
-console.log(updatedCheckSortedAndDiff([1,6,7,8,9])) // 0 -> 1
-console.log(updatedCheckSortedAndDiff([1,4,4,6,8])) // 0 -> 1
-console.log(updatedCheckSortedAndDiff([1,3,2,4,5])) // 0 -> 1
-console.log(updatedCheckSortedAndDiff([1,2,3,4,0])) // 0 -> 1
-console.log(updatedCheckSortedAndDiff([53, 54, 57, 58, 59, 61, 65])) // 0 -> 1
+// Tests
+// console.log(updatedCheckSortedAndDiff([1,2,4,6,7])) // 1
+// console.log(updatedCheckSortedAndDiff([1,2,6,7,8])) // 0
+// console.log(updatedCheckSortedAndDiff([1,6,7,8,9])) // 0 -> 1
+// console.log(updatedCheckSortedAndDiff([1,4,4,6,8])) // 0 -> 1
+// console.log(updatedCheckSortedAndDiff([1,3,2,4,5])) // 0 -> 1
+// console.log(updatedCheckSortedAndDiff([1,8,2,4,5])) // 0 -> 1
+// console.log(updatedCheckSortedAndDiff([1,2,3,4,0])) // 0 -> 1
+// console.log(updatedCheckSortedAndDiff([53, 54, 57, 58, 59, 61, 65])) // 0 -> 1
