5671000d4304eb9885093550d252a6d60e64cfca
Tamara Krafft <tamara.krafft@uni-a.de>
2024-12-02 11:51:12 +0100

    nicer logic for star one

--- 2024/day02/day02.ts
+++ 2024/day02/day02.ts
@@ -7,28 +7,34 @@ function parse(data: string): number[][] {
         .map(line => line.split(" ").map(Number));
 }
 
-function checkSortedAndDiff(arr: number[]): number {
-    let i = 0;
-    while (i < arr.length - 1) {
-        if (arr[i] > arr[i + 1]) {
-            return 0;
-        } else {
-            if (0 == (arr[i] - arr[i + 1]) || Math.abs(arr[i] - arr[i + 1]) > 3) {
-                return 0;
-            }
-        }
-        i++;
+function diff(arr: number[]): number[] {
+    if (arr.length < 2) {
+        return []; // Return an empty array if there are less than 2 elements
     }
+    return arr.slice(1).map((value, index) => value - arr[index]);
+}
+
+function checkSortedAndDiff(arr: number[]): number {
+    let diffArr = diff(arr);
+    if (diffArr.some((a) => Math.abs(a) > 3 || a === 0)) {
+        return 0;
+    }
+
+    const hasBothDirections = diffArr.some((a) => a > 0) && diffArr.some((a) => a < 0);
+    if (hasBothDirections) {
+        return 0;
+    }
+
     return 1;
+}
+
+function updatedCheckSortedAndDiff(arr: number[]): number {
+    return 0;
 }
 
 function solveStarOne(levels: number[][]): number {
     let countSafeLevels = 0;
     for (let level of levels) {
-        // Stupid sorting logic but works for no tolerance strategy
-        if (level[0] > level[level.length - 1]) {
-            level.reverse()
-        }
         countSafeLevels += checkSortedAndDiff(level)
     }
     return countSafeLevels;
@@ -36,6 +42,9 @@ function solveStarOne(levels: number[][]): number {
 
 function solveStarTwo(levels: number[][]): number {
     let countSafeLevels = 0;
+    for (let level of levels) {
+        countSafeLevels += updatedCheckSortedAndDiff(level)
+    }
     return countSafeLevels;
 }
 
@@ -49,9 +58,10 @@ try {
     console.error(err);
 }
 
-console.log(checkSortedAndDiff([1,2,4,6,7])) // 1
-console.log(checkSortedAndDiff([1,2,6,7,8])) // 0
-console.log(checkSortedAndDiff([1,6,7,8,9])) // 0 -> 1
-console.log(checkSortedAndDiff([1,4,4,6,8])) // 0 -> 1
-console.log(checkSortedAndDiff([1,3,2,4,5])) // 0 -> 1
-console.log(checkSortedAndDiff([53, 54, 57, 58, 59, 61, 65])) // 0 -> 1
+console.log(updatedCheckSortedAndDiff([1,2,4,6,7])) // 1
+console.log(updatedCheckSortedAndDiff([1,2,6,7,8])) // 0
+console.log(updatedCheckSortedAndDiff([1,6,7,8,9])) // 0 -> 1
+console.log(updatedCheckSortedAndDiff([1,4,4,6,8])) // 0 -> 1
+console.log(updatedCheckSortedAndDiff([1,3,2,4,5])) // 0 -> 1
+console.log(updatedCheckSortedAndDiff([1,2,3,4,0])) // 0 -> 1
+console.log(updatedCheckSortedAndDiff([53, 54, 57, 58, 59, 61, 65])) // 0 -> 1
