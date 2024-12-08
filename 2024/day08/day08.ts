8ea65e15723c6f46c7d9f7e5ea129c4e25510015
Tamara Krafft <tamara.krafft@uni-a.de>
2024-12-08 14:27:35 +0100

    day08 star two

--- 2024/day08/day08.ts
+++ 2024/day08/day08.ts
@@ -41,12 +41,54 @@ function solveStarOne(input: string[][]): number {
         }
     }
 
-    console.log(antinodes)
     return antinodes.length;
 }
 
 function solveStarTwo(input: string[][]): number {
-    return 0;
+    let antinodes = new Array<number[]>();
+    for (let row=0; row < input.length; row++) {
+        for (let col=0; col < input[0].length; col++) {
+            let currentLetter = input[row][col];
+            if (currentLetter != '.') {
+                for (let i=0; i < input.length; i++) {
+                    for (let j=0; j < input[0].length; j++) {
+                        if (i!==row && j!==col && input[i][j] == currentLetter) {
+                            antinodes.push([row, col]);
+                            let xDiff = i - row;
+                            let yDiff = j - col;
+                            let newAntinodeOne = [i + xDiff, j + yDiff];
+                            let k = 1;
+                            while (row - xDiff * k >= 0 && row - xDiff * k < input.length
+                                && col - yDiff * k >= 0 && col - yDiff * k < input[0].length
+                            ) {
+                                newAntinodeOne = [row - xDiff * k, col - yDiff * k];
+                                k++;
+                                antinodes.push(newAntinodeOne);
+                            }
+
+                            let newAntinodeTwo = [i + xDiff, j + yDiff];
+                            let l = 1;
+                            while (row - xDiff * l >= 0 && row - xDiff * l < input.length
+                                && col - yDiff * l >= 0 && col - yDiff * l < input[0].length
+                            ) {
+                                newAntinodeTwo = [row - xDiff * l, col - yDiff * l];
+                                l++;
+                                antinodes.push(newAntinodeTwo);
+                            }
+                        }
+                    }
+                }
+
+            }
+        }
+    }
+
+    // Remove duplicates
+    const uniqueAntinodes = Array.from(
+        new Set(antinodes.map(node => JSON.stringify(node))) // Convert arrays to strings for comparison
+    ).map(str => JSON.parse(str)); // Convert strings back to arrays
+
+    return uniqueAntinodes.length;
 }
 
 try {
