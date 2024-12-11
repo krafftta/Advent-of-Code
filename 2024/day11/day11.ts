ffd1b380bffd12a6e4caaac26f098ec37dce275a
Tamara Krafft <tamara.krafft@uni-a.de>
2024-12-11 18:08:46 +0100

    day11

--- 2024/day11/day11.ts
+++ 2024/day11/day11.ts
@@ -40,46 +40,7 @@ function solveStarOne(stones: number[]): number {
 }
 
 function solveStarTwo(originalStones: number[]): number {
-    let allStones = [originalStones];
-    for (let i = 0; i < blinks; i++) {
-        if (originalStones.length >= 40000000){
-            const midpoint = Math.floor(originalStones.length / 2);
-            allStones = [originalStones.slice(0, midpoint), originalStones.slice(midpoint)]
-        }
-
-        for (let stones of allStones) {
-            const newStones: number[] = [];
-            console.log(i, stones.length)
-            for (let sId = 0; sId < stones.length; sId++) {
-                const s = stones[sId];
-                if (s === 0) {
-                    // Replace 0 with 1
-                    newStones.push(1);
-                } else {
-                    const sStr = s.toString();
-                    if (sStr.length % 2 === 0) {
-                        // Even length: split into two numbers
-                        const half = sStr.length / 2;
-                        newStones.push(
-                            parseInt(sStr.substring(0, half), 10),
-                            parseInt(sStr.substring(half), 10)
-                        );
-                    } else {
-                        // Odd length: multiply by 2024
-                        newStones.push(s * 2024);
-                    }
-                }
-            }
-            // Replace the old array with the transformed one
-            stones = newStones;
-        }
-
-    }
-
     let result = 0;
-    for (let st of allStones) {
-        result += st.length
-    }
     return result;
 }
 
